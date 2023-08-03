d7b394ec8ec59f5759864ef040f1646b03d077fd
Oscar Parra Rodriguez <oprodriguez1990@gmail.com>
2023-08-03 16:11:00 +0200

    Validando que no haya tareas con el texto repetido

--- src/components/TareaForm.vue
+++ src/components/TareaForm.vue
@@ -1,42 +1,58 @@
-<!-- TareaForm.vue -->
 <template>
     <form @submit.prevent="agregarTarea">
         <input type="text" class="form-control my-3" placeholder="Ingrese tarea" v-model.trim="texto" required>
+        <p v-if="mensajeError" class="alert alert-danger">{{ mensajeError }}</p>
     </form>
 </template>
   
 <script>
-/* eslint-disable */
 import { inject, ref } from 'vue';
+
 export default {
     name: "Tarea-Form",
 
     setup() {
-        const tareas = inject('arregloTareas')
-        const texto = ref('')
-        const agregarTarea = () => {
+        const tareas = inject('arregloTareas');
+        const texto = ref('');
+        const mensajeError = ref('');
 
+        const agregarTarea = () => {
             if (texto.value === '') {
-                console.log('Uff, necesitas escribir algo')
-                return
+                mostrarMensajeError('Uff, necesitas escribir algo');
+                return;
+            }
+
+            // Verificar que esa tarea no exista (por el texto)
+            const tareaExistente = tareas.value.find(tarea => tarea.texto === texto.value);
+            if (tareaExistente) {
+                mostrarMensajeError('La tarea ya existe');
+                return;
             }
 
             const tarea = {
                 texto: texto.value,
                 estado: false,
                 id: Date.now()
+            };
 
-            }
+            tareas.value.push(tarea);
+            texto.value = '';
+        };
 
-            tareas.value.push(tarea)
-            texto.value = ''
-        }
+        const mostrarMensajeError = (mensaje) => {
+            mensajeError.value = mensaje;
+            setTimeout(() => {
+                mensajeError.value = '';
+            }, 1500);
+        };
 
         return {
             agregarTarea,
             texto,
-        }
-    }
-
+            mensajeError,
+        };
+    },
 };
 </script>
+
+  
